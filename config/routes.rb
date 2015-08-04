f1ec900be527889d0ce79d925ff6882d254babdd
Max Tedford <max.tedford@gmail.com>
2015-08-03 20:35:18 -0600

    homepage finished and tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   
+  get '/login', as: :login, to: 'sessions#new'
+  
   root 'welcome#index'
 end
--- spec/feature/unauth_user_spec.rb
+++ spec/feature/unauth_user_spec.rb
@@ -1,20 +1,27 @@
 require 'rails_helper'
 
 describe 'the unauthenticated user', type: :feature do
+  
+  before(:each) do
+    visit root_path
+  end
+  
   context 'visits the root page' do
 
     it 'renders the root page' do
-      visit root_path
-
       expect(page).to have_http_status(:ok)
     end
 
-    xit 'displays the welcome message' do
-      visit root_path
-
-      expect(page).to have_content("Digital Cellar")
-      expect(page).to have_content("Your personal wine inventory solution")
+    it 'displays the welcome message' do
+      expect(page).to have_content("RoadTripper")
+      expect(page).to have_content("Helping you discover the best your road trip has to offer")
       expect(page).to have_link("Get Started!")
+    end
+    
+    it 'redirects to the login page when user clicks the button' do
+      click_link "Get Started!"
+      
+      expect(current_path).to eq(login_path)
     end
   end
 end
