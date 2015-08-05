1c224089fead30930b9d846d2652948e0c1b1062
Max Tedford <max.tedford@gmail.com>
2015-08-05 13:12:07 -0600

    more routes troubleshoot

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  force_ssl :only => :callback
   
   get '/dashboard', as: :dashboard, to: 'dashboard#show'
   get '/auth/google_oauth2', as: :login
-  get '/auth/google_oauth2/callback', to: 'sessions#create', constraints: { protocol: "https" }
+  get '/auth/google_oauth2/callback', to: 'sessions#create', as: :callback
   get '/logout', as: :logout, to: 'sessions#destroy'
   
   root 'welcome#index'
