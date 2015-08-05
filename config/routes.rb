b02c6226da85bba632e11da589b0c929409ca7dc
Max Tedford <max.tedford@gmail.com>
2015-08-05 10:43:58 -0600

    tweak routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   get '/dashboard', as: :dashboard, to: 'dashboard#show'
   get '/auth/google_oauth2', as: :login
-  get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', as: :logout, to: 'sessions#destroy'
   
   root 'welcome#index'
