75192188abddf0948ff6b91036a8f08642f03f2b
Max Tedford <max.tedford@gmail.com>
2015-08-05 13:04:01 -0600

    more heroku route troubleshoot

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
   get '/dashboard', as: :dashboard, to: 'dashboard#show'
-  get '/auth/google_oauth2', as: :login, ssl_only: true
-  get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get '/auth/google_oauth2', as: :login
+  get '/auth/google_oauth2/callback', to: 'sessions#create', ssl_only: true
   get '/logout', as: :logout, to: 'sessions#destroy'
   
   root 'welcome#index'
