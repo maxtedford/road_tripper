26d91ef70ac192a3cefe12bc98c81ae4e2819388
Max Tedford <max.tedford@gmail.com>
2015-08-09 14:03:39 -0600

    add geocoding and searchPlaces methods

--- app/assets/javascripts/directions.js
+++ app/assets/javascripts/directions.js
@@ -1,32 +1,60 @@
 $(document).ready( function() {
   
   $('#route-submit').on('click', function() {
+    var map;
+    var service;
     var directionsDisplay = new google.maps.DirectionsRenderer();
     var directionsService = new google.maps.DirectionsService();
-    var map;
+    var geocoder = new google.maps.Geocoder();
+    var startPoint = document.getElementById("origin-field").value;
+    var endPoint = document.getElementById("destination-field").value;
+    var origin = codePoint(startPoint, geocoder);
+    var destination = codePoint(endPoint, geocoder);
     initialize();
-    displayRoute();
+    displayRoute(startPoint, endPoint, directionsService, directionsDisplay);
 
+    
     function initialize() {
       map = new google.maps.Map(document.getElementById("map-canvas"));
       directionsDisplay.setMap(map);
     }
-
-    function displayRoute() {
-
-      var start = document.getElementById("origin-field").value;
-      var end = document.getElementById("destination-field").value;
-
-      var request = {
-        origin : start,
-        destination : end,
-        travelMode : google.maps.TravelMode.DRIVING
-      };
-      directionsService.route(request, function(response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(response);
-        }
-      });
-    }
   });
 });
+
+function codePoint(input, geocoder) {
+  geocoder.geocode( { 'address': input }, function(results, status) {
+    debugger;
+    if (status == google.maps.GeocoderStatus.OK) {
+      return new google.maps.LatLng(results[0].geometry.location["G"], results[0].geometry.location["K"]);
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+
+function displayRoute(startPoint, endPoint, directionsService, directionsDisplay) {
+  var request = {
+    origin : startPoint, 
+    destination : endPoint,
+    travelMode : google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
+function searchPlaces(service) {
+  console.log(origin);
+  service = new google.maps.places.PlacesService();
+  var searchArea = new google.maps.LatLngBounds(origin, destination);
+  var request = {
+    bounds:searchArea,
+    types:["amusement_park", "aquarium", "art_gallery", "museum", "stadium", "zoo"]
+  };
+  service.radarSearch(request, function(response, status) {
+    if (status == google.maps.PlacesServiceStatus.OK) {
+    }
+  })
+}
