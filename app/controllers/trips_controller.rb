fa26523d6f92355d61a288676cfb1d0239ce891c
Max Tedford <max.tedford@gmail.com>
2015-09-02 12:57:13 -0600

    update readme

--- app/controllers/trips_controller.rb
+++
@@ -1,8 +0,0 @@
-class TripsController < ApplicationController
-  
-  def new
-    gon.origin      = params[:origin]
-    gon.destination = params[:destination]
-    redirect_to dashboard_path
-  end
-end
